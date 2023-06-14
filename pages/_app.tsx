7fef0423b862514fab23ae3ebd201f93ce10aa12
Wesdell <wesdell17@gmail.com>
2023-06-14 10:54:42 -0500

    Providing cart state to _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
 import type { AppProps } from 'next/app';
 
+import { Toaster } from 'react-hot-toast';
+
+import { CartProvider } from '@/context';
 import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <CartProvider>
+      <Toaster />
+      <Component {...pageProps} />
+    </CartProvider>
+  );
 }
